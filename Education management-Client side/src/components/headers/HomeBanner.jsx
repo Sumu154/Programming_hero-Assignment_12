56ab3f3e635eeb40acc09eee4d36fea925929022
Sumu154 <sumaiyatasnim5548@gmail.com>
2025-02-18 00:11:36 +0600

    home banner is created with react swiper according to figma

--- Education management-Client side/src/components/headers/HomeBanner.jsx
+++ Education management-Client side/src/components/headers/HomeBanner.jsx
@@ -18,20 +18,23 @@ const HomeBanner = () => {
   const images = [banner1, banner2, banner3, banner4];
 
 
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [mainSwiper, setMainSwiper] = useState(null);
 
 
   return (
     <div className='relative w-full'>
       {/* main carousel */}
       <div className=' '>
-        <Swiper className='' modules={[Navigation, Thumbs]} thumbs={{ swiper: thumbsSwiper }} navigation={true} loop={true}  slidesPerView={1} >
+        <Swiper className='' modules={[Navigation, Thumbs]}  navigation={true}  slidesPerView={1} onSwiper={setMainSwiper} onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)} >
           {/* images gula ekta ekta kore map kore dekhabo */}
           {images.map((it, index) => {
             return (
-              <SwiperSlide className='' key={index}>
-                <div className='h-[500px] '>
-                  hi
+              <SwiperSlide id='bannerSlide' className='w-full bg-cover bg-center ' style={{backgroundImage: `linear-gradient(259deg, rgba(21, 21, 21, 0.21) 11.86%, rgba(21, 21, 21, 0.49) 56.33%, rgba(21, 21, 21, 0.70) 96.32%), url(${it})`}} key={index}>
+                <div className='w-[90%] mx-auto text-white px-4 pt-24 md:pt-28 lg:pt-32 pb-16 md:pb-20 lg:pb-28   '>
+                  <p className='font-Montserrat font-semibold text-lg md:text-xl pb-2  '> Skill Learning Made Simple </p>
+                  <h3 className='font-Montserrat text-4xl md:text-5xl lg:text-6xl  font-bold max-w-[850px] pb-4 '> Revolutionizing Education & Skill Learning! </h3>
+                  <p className='text-sm md:text-base max-w-[600px] '> Empowering institutions, tutors, and students with a seamless learning experience. Manage classes, track progress, and enhance skills—all track progress, and enhance skills—all in one place. </p>
                 </div>
               </SwiperSlide>
             )
@@ -39,17 +42,13 @@ const HomeBanner = () => {
         </Swiper>
       </div>
       
-      {/* thumbnail swiper */}
-      <div>
-        <Swiper modules={[Thumbs]} watchSlidesProgress onSwiper={setThumbsSwiper}>
-          {images.map((it, index) => {
-              return (
-                <SwiperSlide className='' key={index}>
-                  <img className='' src={it} alt="" />
-                </SwiperSlide>
-              )
-            })}
-        </Swiper>
+      {/* Thumbnail Swiper */}
+      <div className="my-6 max-w-[420px] md:max-w-[600px] mx-auto grid grid-cols-4 gap-5  ">
+        {images.map((img, index) => (
+          <div key={index} className={`h-[50px] md:h-[65px] cursor-pointer ${activeIndex === index ? 'border-2 md:border-4  border-transparent outline-3 md:outline-4 outline-dark/90' : 'border-transparent'}`} onClick={() => mainSwiper && mainSwiper.slideTo(index)} >
+            <img src={img} alt="" className="w-full h-full object-cover" />
+          </div>
+        ))}
       </div>
     </div>
   );
--- Education management-Client side/src/layouts/MainLayout.jsx
+++ Education management-Client side/src/layouts/MainLayout.jsx
@@ -5,7 +5,7 @@ import Footer from '../components/footers/Footer';
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className='font-OpenSans'>
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
